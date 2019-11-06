da4e2ce0779bb8d0c289293fdb2a8e7244459c93
nbelvoix <belvoixnicolas1997@gmail.com>
2019-11-06 20:49:09 +0100

    deroulement position

--- view/layout/deroulement.php
+++ view/layout/deroulement.php
@@ -1,19 +1,21 @@
 <article id="deroulement">
     <h2>Déroulement</h2>
 
-    <section>
-        <img src="https://via.placeholder.com/100?text=image_déroulement" alt="imagr déroulement">
-        <h3>Titre déroulement</h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam dicta amet est rem voluptatem quos veniam! Perferendis officia quis recusandae, totam vitae porro voluptates exercitationem vero culpa sed a odit nisi magnam quibusdam eum cum. Vel aperiam veniam eos sint eum labore tempore illo dolorum molestias! Fugit deserunt similique quaerat.</p>
-    </section>
-    <section>
-        <img src="https://via.placeholder.com/100?text=image_déroulement" alt="imagr déroulement">
-        <h3>Titre déroulement</h3>
-        <p>Est officia quidem quisquam dignissimos, aspernatur beatae earum laboriosam a quis possimus, ipsa, esse non tempora debitis laudantium architecto omnis hic doloribus nemo. Officia temporibus perspiciatis vitae accusantium labore dolores quae eligendi nulla. Distinctio, minus? Ducimus, blanditiis suscipit? Eum corrupti molestias voluptatibus asperiores labore quibusdam repellendus aperiam voluptatum itaque repudiandae.</p>
-    </section>
-    <section>
-        <img src="https://via.placeholder.com/100?text=image_déroulement" alt="imagr déroulement">
-        <h3>Titre déroulement</h3>
-        <p>Dolores delectus quisquam ipsa natus id quae suscipit sequi blanditiis voluptatum aliquam quod vero rerum asperiores, dolorem recusandae hic ea perferendis nisi magni repudiandae aut eveniet. Cupiditate ea exercitationem dolores beatae accusamus. Id molestias quas, saepe sunt eveniet eos placeat neque obcaecati ad inventore eius porro possimus reprehenderit repudiandae. Pariatur!</p>
-    </section>
+    <div class="etape">
+        <section>
+            <img src="https://via.placeholder.com/100?text=image_déroulement" alt="imagr déroulement">
+            <h3>Titre déroulement</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam dicta amet est rem voluptatem quos veniam! Perferendis officia quis recusandae, totam vitae porro voluptates exercitationem vero culpa sed a odit nisi magnam quibusdam eum cum. Vel aperiam veniam eos sint eum labore tempore illo dolorum molestias! Fugit deserunt similique quaerat.</p>
+        </section>
+        <section>
+            <img src="https://via.placeholder.com/100?text=image_déroulement" alt="imagr déroulement">
+            <h3>Titre déroulement</h3>
+            <p>Est officia quidem quisquam dignissimos, aspernatur beatae earum laboriosam a quis possimus, ipsa, esse non tempora debitis laudantium architecto omnis hic doloribus nemo. Officia temporibus perspiciatis vitae accusantium labore dolores quae eligendi nulla. Distinctio, minus? Ducimus, blanditiis suscipit? Eum corrupti molestias voluptatibus asperiores labore quibusdam repellendus aperiam voluptatum itaque repudiandae.</p>
+        </section>
+        <section>
+            <img src="https://via.placeholder.com/100?text=image_déroulement" alt="imagr déroulement">
+            <h3>Titre déroulement</h3>
+            <p>Dolores delectus quisquam ipsa natus id quae suscipit sequi blanditiis voluptatum aliquam quod vero rerum asperiores, dolorem recusandae hic ea perferendis nisi magni repudiandae aut eveniet. Cupiditate ea exercitationem dolores beatae accusamus. Id molestias quas, saepe sunt eveniet eos placeat neque obcaecati ad inventore eius porro possimus reprehenderit repudiandae. Pariatur!</p>
+        </section>
+    </div>
 </article>
