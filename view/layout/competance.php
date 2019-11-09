c357cbe23bdb7ff9d10cf9ad27d4d3c372092d6d
nbelvoix <belvoixnicolas1997@gmail.com>
2019-11-09 21:32:39 +0100

    competance css

--- view/layout/competance.php
+++ view/layout/competance.php
@@ -1,54 +1,56 @@
 <article id="competance">
     <h2>Compétances</h2>
-    <section>
-        <h3>Titre</h3>
-        <ul>
-            <li>
-                <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non porro rem nemo voluptates! Accusamus ea assumenda aut quo autem sit nihil illo, in architecto? Inventore magnam impedit a cum dolore, ea, maiores nesciunt aut nemo praesentium, consequuntur quis ab pariatur deleniti distinctio molestias eligendi ratione? Nobis commodi impedit fugiat dolores.</p>
-            </li>
-            <li>
-                <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
-                <p>Dolores necessitatibus non ad eligendi deserunt hic. Repellendus, enim. Fuga corrupti impedit ullam qui quam, tempore repellendus eos. Tenetur, fuga voluptates eius porro error atque quae cupiditate assumenda, hic culpa deserunt voluptatum fugiat suscipit rem cumque maiores id sunt, architecto velit obcaecati qui reiciendis odit deleniti modi? Ipsa, sit assumenda!</p>
-            </li>
-            <li>
-                <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
-                <p>At incidunt pariatur perspiciatis nesciunt vel! Exercitationem laudantium assumenda corporis facilis ex omnis enim minima modi ipsam? Earum asperiores quidem commodi aperiam dicta officia iusto vel nesciunt animi ducimus, porro cum laboriosam, assumenda quam nihil voluptatibus modi corrupti velit eos. Aliquid vitae facilis libero velit, earum quasi quaerat ex ipsa.</p>
-            </li>
-        </ul>
-    </section>
-    <section>
-        <h3>Titre</h3>
-        <ul>
-            <li>
-                <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
-                <p>Explicabo eaque vitae dolor ipsam asperiores? Alias aperiam eveniet libero dignissimos sint perferendis placeat eos. Laboriosam, beatae aperiam vel cumque aut neque facere praesentium quam debitis, distinctio dolorum voluptatum! Placeat tempore beatae sunt, porro voluptatem, unde sit aspernatur recusandae vero ipsa sequi nemo blanditiis? Suscipit sit id tempora molestias dolor.</p>
-            </li>
-            <li>
-                <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
-                <p>Architecto doloremque rem hic eius! Ut deleniti delectus recusandae necessitatibus veniam nesciunt incidunt! Nostrum omnis cum qui quisquam exercitationem aliquid expedita culpa officiis maxime facere accusantium eius cumque voluptatibus voluptas debitis laboriosam pariatur ipsum dicta, sequi aperiam, nisi tempore quos illum? Voluptas veritatis sapiente magnam hic commodi, consequuntur alias pariatur!</p>
-            </li>
-            <li>
-                <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
-                <p>Vitae nesciunt ab sunt nostrum nihil. Quae vero, tenetur magnam deserunt dolorum a facere quisquam nihil eius odit dolores officiis fuga blanditiis sint dolorem laborum itaque ipsum. Reiciendis, nulla. Fugiat possimus, eveniet veniam eaque soluta nisi excepturi magni temporibus, placeat ducimus expedita voluptate vitae nam dolor aperiam incidunt inventore ea.</p>
-            </li>
-        </ul>
-    </section>
-    <section>
-        <h3>Titre</h3>
-        <ul>
-            <li>
-                <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
-                <p>Repellat quasi magnam debitis optio, voluptatum aliquid ut, laudantium similique quae quisquam laborum non ducimus voluptatibus inventore ab perspiciatis corrupti tenetur sint fugit consequuntur temporibus nisi vero ratione. Quae dolorem, eaque impedit nisi ut neque corporis quas commodi delectus iste ex, sed rem maiores est nemo doloremque earum facere accusamus.</p>
-            </li>
-            <li>
-                <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
-                <p>Quas, dignissimos officiis velit magnam quidem fuga laudantium non reprehenderit facere modi ipsum totam libero eum itaque voluptate id atque sequi adipisci. Fugit doloremque nulla architecto tempora, dolor expedita magni doloribus libero incidunt vitae repudiandae, omnis officia autem delectus qui tempore impedit dolore nobis quo sint ratione laborum deleniti quasi?</p>
-            </li>
-            <li>
-                <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
-                <p>Quidem vel nihil excepturi illo reiciendis, unde itaque at tempora earum ad dolor inventore nesciunt vitae corporis ab dicta possimus facere iure fugit aperiam. Amet nihil hic dolor, iusto quidem eum, iste odit explicabo saepe ratione aliquid cupiditate totam optio, non nisi corrupti soluta. Quasi officiis id doloremque earum animi.</p>
-            </li>
-        </ul>
-    </section>
+    <div class="categorie">
+        <section>
+            <h3>Titre</h3>
+            <ul>
+                <li>
+                    <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non porro rem nemo voluptates! Accusamus ea assumenda aut quo autem sit nihil illo, in architecto? Inventore magnam impedit a cum dolore, ea, maiores nesciunt aut nemo praesentium, consequuntur quis ab pariatur deleniti distinctio molestias eligendi ratione? Nobis commodi impedit fugiat dolores.</p>
+                </li>
+                <li>
+                    <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
+                    <p>Dolores necessitatibus non ad eligendi deserunt hic. Repellendus, enim. Fuga corrupti impedit ullam qui quam, tempore repellendus eos. Tenetur, fuga voluptates eius porro error atque quae cupiditate assumenda, hic culpa deserunt voluptatum fugiat suscipit rem cumque maiores id sunt, architecto velit obcaecati qui reiciendis odit deleniti modi? Ipsa, sit assumenda!</p>
+                </li>
+                <li>
+                    <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
+                    <p>At incidunt pariatur perspiciatis nesciunt vel! Exercitationem laudantium assumenda corporis facilis ex omnis enim minima modi ipsam? Earum asperiores quidem commodi aperiam dicta officia iusto vel nesciunt animi ducimus, porro cum laboriosam, assumenda quam nihil voluptatibus modi corrupti velit eos. Aliquid vitae facilis libero velit, earum quasi quaerat ex ipsa.</p>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h3>Titre</h3>
+            <ul>
+                <li>
+                    <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
+                    <p>Explicabo eaque vitae dolor ipsam asperiores? Alias aperiam eveniet libero dignissimos sint perferendis placeat eos. Laboriosam, beatae aperiam vel cumque aut neque facere praesentium quam debitis, distinctio dolorum voluptatum! Placeat tempore beatae sunt, porro voluptatem, unde sit aspernatur recusandae vero ipsa sequi nemo blanditiis? Suscipit sit id tempora molestias dolor.</p>
+                </li>
+                <li>
+                    <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
+                    <p>Architecto doloremque rem hic eius! Ut deleniti delectus recusandae necessitatibus veniam nesciunt incidunt! Nostrum omnis cum qui quisquam exercitationem aliquid expedita culpa officiis maxime facere accusantium eius cumque voluptatibus voluptas debitis laboriosam pariatur ipsum dicta, sequi aperiam, nisi tempore quos illum? Voluptas veritatis sapiente magnam hic commodi, consequuntur alias pariatur!</p>
+                </li>
+                <li>
+                    <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
+                    <p>Vitae nesciunt ab sunt nostrum nihil. Quae vero, tenetur magnam deserunt dolorum a facere quisquam nihil eius odit dolores officiis fuga blanditiis sint dolorem laborum itaque ipsum. Reiciendis, nulla. Fugiat possimus, eveniet veniam eaque soluta nisi excepturi magni temporibus, placeat ducimus expedita voluptate vitae nam dolor aperiam incidunt inventore ea.</p>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h3>Titre</h3>
+            <ul>
+                <li>
+                    <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
+                    <p>Repellat quasi magnam debitis optio, voluptatum aliquid ut, laudantium similique quae quisquam laborum non ducimus voluptatibus inventore ab perspiciatis corrupti tenetur sint fugit consequuntur temporibus nisi vero ratione. Quae dolorem, eaque impedit nisi ut neque corporis quas commodi delectus iste ex, sed rem maiores est nemo doloremque earum facere accusamus.</p>
+                </li>
+                <li>
+                    <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
+                    <p>Quas, dignissimos officiis velit magnam quidem fuga laudantium non reprehenderit facere modi ipsum totam libero eum itaque voluptate id atque sequi adipisci. Fugit doloremque nulla architecto tempora, dolor expedita magni doloribus libero incidunt vitae repudiandae, omnis officia autem delectus qui tempore impedit dolore nobis quo sint ratione laborum deleniti quasi?</p>
+                </li>
+                <li>
+                    <h4><img src="https://via.placeholder.com/100?text=image_competance" alt="Image compétance"></h4>
+                    <p>Quidem vel nihil excepturi illo reiciendis, unde itaque at tempora earum ad dolor inventore nesciunt vitae corporis ab dicta possimus facere iure fugit aperiam. Amet nihil hic dolor, iusto quidem eum, iste odit explicabo saepe ratione aliquid cupiditate totam optio, non nisi corrupti soluta. Quasi officiis id doloremque earum animi.</p>
+                </li>
+            </ul>
+        </section>
+    </div>
 </article>
