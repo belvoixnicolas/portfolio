536209491762ab18a9b051a84f895e30c6216022
nbelvoix <belvoixnicolas1997@gmail.com>
2019-11-09 21:41:52 +0100

    portfolio position

--- view/layout/portfolio.php
+++ view/layout/portfolio.php
@@ -1,24 +1,25 @@
 <article id="portfolio">
     <h2>Portfolio</h2>
-
-    <section>
-        <h3>Titre site</h3>
-        <img src="https://via.placeholder.com/100?text=image_site" alt="image site">
-        <span>tag / tag / tag</span>
-    </section>
-    <section>
-        <h3>Titre site</h3>
-        <img src="https://via.placeholder.com/100?text=image_site" alt="image site">
-        <span>tag / tag / tag</span>
-    </section>
-    <section>
-        <h3>Titre site</h3>
-        <img src="https://via.placeholder.com/100?text=image_site" alt="image site">
-        <span>tag / tag / tag</span>
-    </section>
-    <section>
-        <h3>Titre site</h3>
-        <img src="https://via.placeholder.com/100?text=image_site" alt="image site">
-        <span>tag / tag / tag</span>
-    </section>
+    <div class="sites">
+        <section>
+            <h3>Titre site</h3>
+            <img src="https://via.placeholder.com/100?text=image_site" alt="image site">
+            <span>tag / tag / tag</span>
+        </section>
+        <section>
+            <h3>Titre site</h3>
+            <img src="https://via.placeholder.com/100?text=image_site" alt="image site">
+            <span>tag / tag / tag</span>
+        </section>
+        <section>
+            <h3>Titre site</h3>
+            <img src="https://via.placeholder.com/100?text=image_site" alt="image site">
+            <span>tag / tag / tag</span>
+        </section>
+        <section>
+            <h3>Titre site</h3>
+            <img src="https://via.placeholder.com/100?text=image_site" alt="image site">
+            <span>tag / tag / tag</span>
+        </section>
+    </div>
 </article>
