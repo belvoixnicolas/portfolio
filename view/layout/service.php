e67d8ecc49ab9b6768fccd37bf5b0a4ca6c50383
belvoixnicolas <belvoixnicolas1997@gmail.com>
2020-01-09 13:49:17 +0100

    service update css color et position

--- view/layout/service.php
+++ view/layout/service.php
@@ -1,30 +1,21 @@
 <article id="service">
-    <h2>Services</h2>
+    <div class="main">
+        <h2>Services</h2>
 
-    <section>
-        <img src="https://via.placeholder.com/100?text=image_service" alt="Image service">
-        <h3>Titre service</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet laborum cum commodi id perspiciatis aliquid numquam. Voluptas tenetur dolorem minima nesciunt magnam asperiores error non officiis illo aliquam, delectus aperiam ut qui itaque. Eligendi quisquam incidunt libero, voluptate iure ex fugit magni voluptatibus consequatur ab error sunt distinctio consequuntur qui.</p>
-    </section>
-    <section>
-        <img src="https://via.placeholder.com/100?text=image_service" alt="Image service">
-        <h3>Titre service</h3>
-        <p>Delectus explicabo molestiae, fugit accusamus, ipsam eius sit tempora earum suscipit praesentium voluptatibus voluptates, distinctio ex! Explicabo cum perspiciatis nisi doloribus magnam aspernatur consequuntur, ipsam adipisci, perferendis velit veritatis harum quisquam excepturi sit obcaecati ipsum, quis distinctio architecto natus reprehenderit. Dignissimos, quibusdam veniam provident fugiat officiis porro reiciendis repellat eligendi?</p>
-    </section>
-    <section>
-        <img src="https://via.placeholder.com/100?text=image_service" alt="Image service">
-        <h3>Titre service</h3>
-        <p>Nulla distinctio ipsum modi asperiores, facere soluta laudantium voluptates harum accusamus tenetur dicta tempora quaerat quis nesciunt reprehenderit deleniti? Consequatur, at ipsum. Officia, libero soluta consequatur dolorem expedita ipsum, totam reprehenderit explicabo obcaecati quisquam aliquam veritatis aliquid vero. Asperiores dolores consequuntur ipsum nihil est atque, et cupiditate error. Tempora, dolorem!</p>
-    </section>
-
-    <svg height="0" width="0">
-        <defs>
-            <clipPath id="cross">
-            <rect y="110" x="137" width="90" height="90"/>
-            <rect x="0" y="110" width="90" height="90"/>
-            <rect x="137" y="0" width="90" height="90"/>
-            <rect x="0" y="0" width="90" height="90"/>
-            </clipPath>
-        </defs>
-    </svg>
+        <section>
+            <img src="https://via.placeholder.com/100?text=image_service" alt="Image service">
+            <h3>Titre service</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet laborum cum commodi id perspiciatis aliquid numquam. Voluptas tenetur dolorem minima nesciunt magnam asperiores error non officiis illo aliquam, delectus aperiam ut qui itaque. Eligendi quisquam incidunt libero, voluptate iure ex fugit magni voluptatibus consequatur ab error sunt distinctio consequuntur qui.</p>
+        </section>
+        <section>
+            <img src="https://via.placeholder.com/100?text=image_service" alt="Image service">
+            <h3>Titre service</h3>
+            <p>Delectus explicabo molestiae, fugit accusamus, ipsam eius sit tempora earum suscipit praesentium voluptatibus voluptates, distinctio ex! Explicabo cum perspiciatis nisi doloribus magnam aspernatur consequuntur, ipsam adipisci, perferendis velit veritatis harum quisquam excepturi sit obcaecati ipsum, quis distinctio architecto natus reprehenderit. Dignissimos, quibusdam veniam provident fugiat officiis porro reiciendis repellat eligendi?</p>
+        </section>
+        <section>
+            <img src="https://via.placeholder.com/100?text=image_service" alt="Image service">
+            <h3>Titre service</h3>
+            <p>Nulla distinctio ipsum modi asperiores, facere soluta laudantium voluptates harum accusamus tenetur dicta tempora quaerat quis nesciunt reprehenderit deleniti? Consequatur, at ipsum. Officia, libero soluta consequatur dolorem expedita ipsum, totam reprehenderit explicabo obcaecati quisquam aliquam veritatis aliquid vero. Asperiores dolores consequuntur ipsum nihil est atque, et cupiditate error. Tempora, dolorem!</p>
+        </section>
+    </div>
 </article>
